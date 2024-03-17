74ae9b6751b832786bb59eefc3eea71dd919c448
ubaid Ashraf <ubaidwani133@gmail.com>
2024-03-17 22:25:05 +0530

    webpage created

---
+++ script.js
@@ -0,0 +1,71 @@
+
+let nav = document.querySelector(".navbar");
+let lastscrollY = window.scrollY;
+
+window.addEventListener("scroll",()=>{
+  if(lastscrollY < window.scrollY){
+    nav.classList.add("nav--hidden");
+  }else{
+    nav.classList.remove("nav--hidden");
+  }
+     lastscrollY = window.scrollY;
+});
+
+
+const myslide = document.querySelectorAll('.myslide');
+let counter = 1;
+slidefun(counter);
+
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+}
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  }
+)};
+
